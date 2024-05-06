c36fa7a01b95e556416040c0fb9a546fdb1e8484
Shanky Kalra <shankkalra@gmail.com>
2024-05-05 22:08:02 -0300

    Excel Traverse Done

--- excelDemo.js
+++ excelDemo.js
@@ -1,4 +1,15 @@
 const ExcelJs = require('exceljs');
 
-const workBook = new ExcelJs.workBook();
-const workSheet = workBook.getWorksheet('Sheet1');
+//Workbook
+const workBook = new ExcelJs.Workbook();
+//Read File
+workBook.xlsx.readFile("/Users/shankykalra/Downloads/download.xlsx").then(function () {
+    //Sheet
+    const worksheet = workBook.getWorksheet('Sheet1');
+    //Traversing Row and Cell
+    worksheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, collNumber) => {
+            console.log(cell.value);
+        });
+    });
+})
