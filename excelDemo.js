1273361b8ea8b490ea351f247bf13311235bbfe7
Shanky Kalra <shankkalra@gmail.com>
2024-05-05 22:32:41 -0300

    Excel Operations

--- excelDemo.js
+++ excelDemo.js
@@ -2,7 +2,7 @@ const ExcelJs = require('exceljs');
 
 //Way 1 via Promise
 //Workbook
-const workBook = new ExcelJs.Workbook();
+/*const workBook = new ExcelJs.Workbook();
 //Read File
 workBook.xlsx.readFile("/Users/shankykalra/Downloads/download.xlsx").then(function () {
     //Sheet
@@ -13,12 +13,12 @@ workBook.xlsx.readFile("/Users/shankykalra/Downloads/download.xlsx").then(functi
             console.log(cell.value);
         });
     });
-})
+})*/
 
 
 //Way 2 Via async and await
-
 async function excelTravserse() {
+console.log("Via Function");
     const workBook = new ExcelJs.Workbook();
     //Read File
     await workBook.xlsx.readFile("/Users/shankykalra/Downloads/download.xlsx")
@@ -31,6 +31,35 @@ async function excelTravserse() {
         });
     });
 }
+//excelTravserse();
 
-console.log("Via Function");
-excelTravserse();
+
+//Getting Specific Cell Number and Value
+ 
+let output ={row:1, col:1};
+let appleRow =0;
+
+async function excelGetCoordinates() {
+        const workBook = new ExcelJs.Workbook();
+        //Read File
+        await workBook.xlsx.readFile("/Users/shankykalra/Downloads/download.xlsx")
+        //Sheet
+        const worksheet = workBook.getWorksheet('Sheet1');
+        //Traversing Row and Cell
+        worksheet.eachRow((row, rowNumber) => {
+            row.eachCell((cell, collNumber) => {
+                if(cell.value === 'Banana'){
+                    output.row = rowNumber;
+                    output.col = collNumber;
+                }
+                
+            });
+        });
+
+        //Writing Specific Cell
+        const cellToWrite = worksheet.getCell(output.row,output.col);
+        cellToWrite.value = 'New Avocado';
+        await workBook.xlsx.writeFile("/Users/shankykalra/Downloads/download.xlsx");
+    }
+
+    excelGetCoordinates();
